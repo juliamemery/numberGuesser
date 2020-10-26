bf1249bb0604de0ce40bc5de55efb49acf23a8b3
Julia Emery <juliamemery@gmail.com>
2020-10-26 15:17:02 -0400

    added scoring, repeat plays, and user input range to the game

--- numberGuesser.py
+++ numberGuesser.py
@@ -11,16 +11,32 @@ def giveHint( guessed: int, chosen: int ) -> str:
 
 
 if __name__=='__main__':
-    aInt: int = 1
-    bInt: int = 100
-    seed(random.randint(1,200))
-    chosen: int = random.randint(1,100)
-    print(f"Guess the number between {aInt} and {bInt}:\t" )
-    guessed: int = int(input())
-    while guessed != chosen:
-        hint: str = giveHint( guessed, chosen )
-        print( hint )
-        guessed = int(input())
-    print( f"Correct! {guessed} is the correct number.")
+    aInt: int
+    bInt: int
+    play_again: bool = True
+    while play_again:
+        guesses: int = 1
+        seed(random.randint(1,200))
+        print("Choose a range. Enter lowest number:\t")
+        aInt = int(input())
+        print("Enter highest number:\t")
+        bInt = int(input())
+        while bInt <= aInt:
+            print(f"Enter number higher than {aInt}")
+            bInt = int(input())
+        chosen: int = random.randint(aInt, bInt)
+
+        print(f"Guess the number between {aInt} and {bInt}:\t" )
+        guessed: int = int(input())
+        while guessed != chosen:
+            hint: str = giveHint( guessed, chosen )
+            print( hint )
+            guessed = int(input())
+            guesses += 1
+        print( f"Correct! It took you {guesses} guesses(s) to get {guessed} as the correct number.")
+        print("Play again? [Y/N]")
+        decision = input()
+        if decision == 'N' or decision == 'n':
+            play_again = False
 
     
